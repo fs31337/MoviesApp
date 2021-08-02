ebb87393ce32af3f67b3a7b0ec56cfc9155025d8
Franco Salina <58792994+fs31337@users.noreply.github.com>
2021-08-02 02:21:26 -0300

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 
 export function getMovies(titulo) {
     return function(dispatch) {
-      return fetch("http://www.omdbapi.com/?apikey=20dac387&s=" + titulo)
+      return fetch("https://www.omdbapi.com/?apikey=20dac387&s=" + titulo)
         .then(response => response.json())
         .then(json => {
           dispatch({ type: "GET_MOVIES", payload: json });
@@ -15,7 +15,7 @@ export function addMovieFavorite(payload) {
 
 export function getMovieDetail(payload) {
     return function (dispatch) {
-       return fetch("http://www.omdbapi.com/?apikey=20dac387&i=" + payload)
+       return fetch("https://www.omdbapi.com/?apikey=20dac387&i=" + payload)
         .then(response => response.json())
         .then(json => {
           dispatch({ type: "GET_MOVIE_DETAIL", payload: json });
